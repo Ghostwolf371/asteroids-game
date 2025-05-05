f76adbda0160bcee6f99970e89d658dab86cc23f
Ghostwolf371 <terrencelinger05@gmail.com>
2025-05-04 23:32:43 -0300

    QWERTY(aka a and d rotation)

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import pygame
+from player import Player
 from constants import *
 
 
 def main():
     pygame.init()
 
+    tm = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    player = Player(x=SCREEN_WIDTH / 2, y=SCREEN_HEIGHT / 2)
 
     while True:
 
@@ -14,7 +19,14 @@ def main():
                 return
 
         screen.fill("black")
+
+        player.update(dt)
+
+        player.draw(screen)
+
         pygame.display.flip()
+
+        dt = tm.tick(60) / 1000
 
 
 if __name__ == "__main__":
